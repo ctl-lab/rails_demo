faeafa8cde462920375ddf5d69889d694a941102
emiliano-ma <emilianomainero@gmail.com>
2020-09-08 10:48:21 +0200

    up to index --> to push for heroku

--- spec/features/user_can_see_list_of_articles_spec.rb
+++ spec/features/user_can_see_list_of_articles_spec.rb
@@ -1,1 +1,20 @@
-feature
+require 'rails_helper'
+
+feature 'List articles on index page' do
+  context 'with articles in db' do
+    before do
+      create(:article, title: 'This is going to be a long week')
+      create(:article, title: 'Rails is awesome but confusing')
+
+      visit root_path
+    end
+
+    it 'displays first article title' do
+      expect(page).to have_content 'This is going to be a long week'
+    end 
+
+    it 'displays second article title' do
+      expect(page).to have_content 'Rails is awesome but confusing'
+    end 
+  end
+end
